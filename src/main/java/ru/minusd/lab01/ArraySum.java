8e1813c4b7d9cf5cb9dea5b7374fba4aecb8b443
Дмитрий <50549999+MinusD@users.noreply.github.com>
2022-09-02 20:05:21 +0300

    Added Lab01.2 (HarmonicSeries)

--- src/main/java/ru/minusd/lab01/ArraySum.java
+++ src/main/java/ru/minusd/lab01/ArraySum.java
@@ -5,35 +5,35 @@ import java.util.Scanner;
 public class ArraySum {
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
-        int n = scanner.nextInt();
+        int n = scanner.nextInt(); // Array size
         int[] data = new int[n];
         int sum = 0;
-
         int i;
-        for(i = 0; i < n; ++i) {
+        // Input array
+        for (i = 0; i < n; ++i) {
             data[i] = scanner.nextInt();
         }
-
-        for(i = 0; i < n; ++i) {
+        // Cycle for
+        for (i = 0; i < n; ++i) {
             sum += data[i];
         }
-
         System.out.println(sum);
+
+        // Cycle While
         sum = 0;
-
-        for(i = 0; i < n; ++i) {
+        for (i = 0; i < n; ++i) {
             sum += data[i];
         }
-
         System.out.println(sum);
+
+        // Cycle do-while
         i = 0;
         sum = 0;
-
         do {
             sum += data[i];
             ++i;
-        } while(i < n);
-
+        } while (i < n);
         System.out.println(sum);
     }
 }
+
